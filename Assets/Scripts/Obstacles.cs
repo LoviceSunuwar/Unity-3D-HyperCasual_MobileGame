810a49e3297f4ebed6e7f7d2879d8f5a2d4e3b28
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2023-12-28 13:46:23 -0500

    Added Scripts for the obstacles to move towards the player and update the score if the obstacles touches the out cube
    Use of Gameobject.find to find the game object, use of delta position and speed to calculate the speed of the obstacles that are coming towards the player.
    Use of OnCollisionEnter function that is provided by Unity game engine which has allows us to manipulate the object that have made collision,
    like destory

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Obstacles : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public TextMeshProUGUI text; // This allows us to access the text created on text mess pro
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>(); // Gameobject.find serarches for the game object from it's name.
+        // we need to have a closure saying that after finding the scoretext get the component which is to be textmeshproUGUI
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Translate function takes direction which is vector3
+        // Time.deltaTime will allow us to calibrate the speed, depending upon the hardware it is running.
+        // SO when we multiply by deltatime it will be same no matter the hardware
+        transform.Translate(Vector3.back * Time.deltaTime * speed); // By using back the cube will move towawrds the player object
+    }
+
+    private void OnCollisionEnter(Collision collision)
+        // This is a reserve function in Unity and it fires when the collision is detected
+        // it returns the collision <- The object that we collided with
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(collision.gameObject); // we are looking for collision in gameobject that is named Player and when a collision occurs,
+            // we will destroy the given game object in this case, it is the player
+        }
+
+        if (collision.gameObject.name == "Out")
+        {
+            int newScore = int.Parse(text.text) + 1;// we are parsing the int value to the text
+            text.text = newScore.ToString(); // Making the current score the newscore
+            Destroy(gameObject); // Now here, we are not destroying the collided gameobject but rather the game object which will be the obstacle not the Out box
+        }
+    }
+}
